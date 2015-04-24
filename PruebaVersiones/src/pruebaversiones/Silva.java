a0aad8c2468d1534539c092e0e0eeea08b57e729
PC28 <PC28@MAESTRIA18-PC>
2015-04-24 11:28:34 -0500

    cambiotnd

--- PruebaVersiones/src/pruebaversiones/Silva.java
+++ PruebaVersiones/src/pruebaversiones/Silva.java
@@ -14,5 +14,6 @@ public class Silva {
    public static void main(String[] args) {
         System.out.println("hola");
         System.out.println("Hola Ramos");
+        System.out.println("Hola Tandazo");
     } 
 }
